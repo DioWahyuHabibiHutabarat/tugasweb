e64cbf7767ffa35ca6ddf728f78208a7f444fa84
Dio Wahyu Habibi Hutabarat <dwhabibie@gmail.com>
2021-06-03 22:13:08 +0700

    tugas1

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React,{useState} from "react";
+
+function App(){
+  const[panjang, setpanjang] = useState("")
+  const[lebar, setlebar] = useState("")
+  const[hasil, sethasil] = useState("")
+
+  console.log(`panjang : ${panjang}`)
+  console.log(`lebar : ${lebar}`)
+
+  function luas(){
+    sethasil(panjang * lebar)
+  }
+  
+  return(
+    <div class = "App">
+      
+      <p>
+        Masukkan nilai panjang
+      </p>
+      <input type = "text" value = {panjang} onChange = {e=> setpanjang(+e.target.value)}></input>
+      
+      <p>
+        Masukkan nilai lebar
+      </p>
+      <input type = "text" value = {lebar} onChange = {e=> setlebar(+e.target.value)}></input>
+      
+      <p>
+        <button onClick = {luas}>hasil</button>
+      </p>
+
+      <p>
+        {hasil}
+      </p>
+
+    </div>
+  );
+
+}
+export default App;
